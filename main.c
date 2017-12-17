2dcd495905f89649b6784da3d41168daf3266ad1
mikspe <msperkowski@airmail.cc>
2017-12-17 20:12:31 +0100

    Changes to be committed: 	modified:   main.c

---
+++ main.c
@@ -0,0 +1,81 @@
+#define _USE_MATH_DEFINES
+#include<math.h>
+#include<stdio.h>
+#include<string.h>
+
+extern "C" {
+#include"./SDL-2.0.7/include/SDL.h"
+#include"./SDL-2.0.7/include/SDL_main.h"
+}
+
+#define SCREEN_WIDTH	640
+#define SCREEN_HEIGHT	480
+
+// draw a text txt on surface screen, starting from the point (x, y)
+// charset is a 128x128 bitmap containing character images
+void DrawString(SDL_Surface *screen, int x, int y, const char *text, SDL_Surface *charset)
+{
+	int px, py, c;
+	SDL_Rect s, d;
+	s.w = 8;
+	s.h = 8;
+	d.w = 8;
+	d.h = 8;
+	while(*text)
+	{
+		c = *text & 255;
+		px = (c % 16) * 8;
+		py = (c / 16) * 8;
+		s.x = px;
+		s.y = py;
+		d.x = x;
+		d.y = y;
+		SDL_BlitSurface(charset, &s, screen, &d);
+		x += 8;
+		text++;
+	}
+}
+
+// (x, y) is the center of sprite on screen
+void DrawSurface(SDL_Surface *screen, SDL_Surface *sprite, int x, int y) {
+	SDL_Rect dest;
+	dest.x = x - sprite->w / 2;
+	dest.y = y - sprite->h / 2;
+	dest.w = sprite->w;
+	dest.h = sprite->h;
+	SDL_BlitSurface(sprite, NULL, screen, &dest);
+	};
+
+	// draw a single pixel
+void DrawPixel(SDL_Surface *surface, int x, int y, Uint32 color) {
+	int bpp = surface->format->BytesPerPixel;
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+	*(Uint32 *)p = color;
+	};
+
+	// draw a vertical (when dx = 0, dy = 1) or horizontal (when dx = 1, dy = 0) line
+void DrawLine(SDL_Surface *screen, int x, int y, int l, int dx, int dy, Uint32 color) {
+	for(int i = 0; i < l; i++) {
+		DrawPixel(screen, x, y, color);
+		x += dx;
+		y += dy;
+		};
+	};
+
+// draw a rectangle of size l by k
+void DrawRectangle(SDL_Surface *screen, int x, int y, int l, int k,
+                   Uint32 outlineColor, Uint32 fillColor) {
+	int i;
+	DrawLine(screen, x, y, k, 0, 1, outlineColor);
+	DrawLine(screen, x + l - 1, y, k, 0, 1, outlineColor);
+	DrawLine(screen, x, y, l, 1, 0, outlineColor);
+	DrawLine(screen, x, y + k - 1, l, 1, 0, outlineColor);
+	for(i = y + 1; i < y + k - 1; i++)
+		DrawLine(screen, x + 1, i, l - 2, 1, 0, fillColor);
+	};	
+
+int main(int argc, char const *argv[])
+{
+
+	return 0;
+}
