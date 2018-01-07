0de5fbf941eea998a815f15f2af56c54ffdcaa01
mikspe <msperkowski@airmail.cc>
2018-01-07 14:15:55 +0100

    level selector in progress

--- main.c
+++ main.c
@@ -8,11 +8,98 @@
 
 //todo:
 //level selector
+//select_box in menu
 //leaderboard
+
+void select_box(const char *text, int highlight, int n, SDL_Surface *screen, SDL_Surface *charset, int blue, int green)
+{
+	if (highlight == n)
+	{
+		DrawRectangle(screen, 4, 4 + n * TILE + n * 2, SCREEN_WIDTH - 8, TILE, green, green);
+	}
+	else
+	{			
+		DrawRectangle(screen, 4, 4 + n * TILE + n * 2, SCREEN_WIDTH - 8, TILE, blue, blue);
+	}
+	DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, 13 + n * TILE + n * 2, text, charset);
+}
 
 int level_selector(SDL_Surface *screen, SDL_Texture *scrtex, SDL_Renderer *renderer, SDL_Surface *charset, int blue, int black, int green)
 {
-	return 1337;
+	FILE *level_list = fopen("./levels/levels.ini", "r");
+	int amount_levels;
+	fscanf(level_list, "%d;", &amount_levels);
+
+	int *levels;
+	levels = (int *) malloc(amount_levels * sizeof(int));
+
+	for (int i = 0; i < amount_levels; ++i)
+	{	
+		fscanf(level_list, "%d,", &levels[i]);
+	}
+	fclose(level_list);
+
+	SDL_Event event;
+	int choice = 0;
+	int highlight = 1;
+	while(choice == 0)
+	{
+		SDL_FillRect(screen, NULL, black);
+
+		DrawRectangle(screen, 4, 4, SCREEN_WIDTH - 8, TILE, blue, blue);
+		DrawString(screen, screen->w / 2 - strlen("CHOOSE LEVEL") * 8 / 2, 11, "CHOOSE LEVEL", charset);
+
+		for (int i = 0; i < amount_levels; ++i)
+		{
+			char text[16];
+			sprintf(text, "%d", levels[i]);
+			select_box(text, highlight, i + 1, screen, charset, blue, green);
+		}
+
+		SDL_UpdateTexture(scrtex, NULL, screen->pixels, screen->pitch);
+		SDL_RenderCopy(renderer, scrtex, NULL, NULL);
+		SDL_RenderPresent(renderer);
+
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_DOWN:
+							if (highlight < amount_levels)
+							{
+								++highlight;
+							}
+							break;
+						case SDLK_UP:
+							if (highlight > 1)
+							{
+								--highlight;
+							}
+							break;
+						case SDLK_RETURN:
+							switch (highlight)
+							{
+								//tu coś wymyśleć dynamicznego
+								case 1:
+									return 1;
+									break;
+								case 2:
+								case 3:
+									choice = 1;
+									break;
+							}
+							break;
+					}
+					break;
+			}
+		}
+	}
+
+	free(levels);
+	return 6;
 }
 
 void menu(SDL_Surface *screen, SDL_Texture *scrtex, SDL_Renderer *renderer, SDL_Surface *charset, int blue, int black, int green, int &level, int &quit)
@@ -36,7 +123,7 @@ void menu(SDL_Surface *screen, SDL_Texture *scrtex, SDL_Renderer *renderer, SDL_
 			DrawRectangle(screen, 4, 4 + TILE + 2, SCREEN_WIDTH - 8, TILE, blue, blue);
 		}
 		DrawString(screen, screen->w / 2 - strlen("CHOOSE LEVEL") * 8 / 2, 13 + TILE, "CHOOSE LEVEL", charset);
-		
+
 		if (highlight == 2)
 		{
 			DrawRectangle(screen, 4, 4 + 2 * TILE + 2 * 2, SCREEN_WIDTH - 8, TILE, green, green);
